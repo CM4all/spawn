ac346744d4f292f3d6f15e9721dbd208fa536b38
Max Kellermann <mk@cm4all.com>
2017-08-21 13:01:49 +0200

    Agent: use a scope whitelist

--- src/Agent.cxx
+++ src/Agent.cxx
@@ -5,10 +5,37 @@
 #include "Agent.hxx"
 #include "odbus/Error.hxx"
 #include "odbus/Connection.hxx"
+#include "util/StringCompare.hxx"
+
+/**
+ * These systemd scopes are allocated by our software which uses the
+ * process spawner.  Their cgroups are managed by this daemon.
+ */
+static constexpr const char *managed_scopes[] = {
+    "/system.slice/cm4all-beng-spawn.scope/",
+    "/system.slice/cm4all-workshop-spawn.scope/",
+    "/system.slice/cm4all-openssh.scope/",
+};
+
+static const char *
+GetManagedSuffix(const char *path)
+{
+    for (const char *i : managed_scopes) {
+        const char *suffix = StringAfterPrefix(path, i);
+        if (suffix != nullptr)
+            return suffix;
+    }
+
+    return nullptr;
+}
 
 static void
 AgentCgroupReleased(const char *path)
 {
+    const char *suffix = GetManagedSuffix(path);
+    if (suffix == nullptr)
+        return;
+
     printf("Cgroup released: %s\n", path);
 
     // TODO: implement
