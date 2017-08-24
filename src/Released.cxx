b785692c9448eaaeea0578bf80db5a3b76bec0c7
Max Kellermann <mk@cm4all.com>
2017-08-24 11:38:41 +0200

    Released: log cgroup statistics, very rough first draft

--- src/Released.cxx
+++ src/Released.cxx
@@ -3,13 +3,17 @@
  */
 
 #include "Instance.hxx"
+#include "system/Error.hxx"
+#include "io/UniqueFileDescriptor.hxx"
 #include "util/StringCompare.hxx"
 #include "util/ScopeExit.hxx"
+#include "util/PrintException.hxx"
 
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <inttypes.h>
 
 /**
  * These systemd scopes are allocated by our software which uses the
@@ -33,11 +37,87 @@ GetManagedSuffix(const char *path)
 	return nullptr;
 }
 
+static UniqueFileDescriptor
+OpenCgroupFile(const char *relative_path, const char *controller_name,
+	       const char *filename)
+{
+	char path[4096];
+	snprintf(path, sizeof(path), "/sys/fs/cgroup/%s%s/%s.%s",
+		 controller_name, relative_path,
+		 controller_name, filename);
+
+	UniqueFileDescriptor fd;
+	if (!fd.OpenReadOnly(path))
+		throw FormatErrno("Failed to open %s", path);
+	return fd;
+}
+
+static uint64_t
+ReadCgroupNumber(const char *relative_path, const char *controller_name,
+		 const char *filename)
+{
+	auto fd = OpenCgroupFile(relative_path, controller_name, filename);
+	char data[64];
+	ssize_t nbytes = fd.Read(data, sizeof(data) - 1);
+	if (nbytes < 0)
+		throw MakeErrno("Failed to read");
+
+	data[nbytes] = 0;
+
+	char *endptr;
+	auto value = strtoull(data, &endptr, 10);
+	if (endptr == data)
+		throw std::runtime_error("Failed to parse number");
+
+	return value;
+}
+
+static void
+CollectCgroupStats(const char *relative_path, const char *suffix)
+{
+	// TODO: blkio
+	// TODO: multicast statistics
+
+	char buffer[4096];
+	size_t position = 0;
+
+	try {
+		position += sprintf(buffer + position, " cpuacct.usage=%" PRIu64,
+				    ReadCgroupNumber(relative_path, "cpuacct", "usage"));
+	} catch (...) {
+		PrintException(std::current_exception());
+	}
+
+	try {
+		position += sprintf(buffer + position, " cpuacct.usage_user=%" PRIu64,
+				    ReadCgroupNumber(relative_path, "cpuacct", "usage_user"));
+	} catch (...) {
+		PrintException(std::current_exception());
+	}
+
+	try {
+		position += sprintf(buffer + position, " cpuacct.usage_sys=%" PRIu64,
+				    ReadCgroupNumber(relative_path, "cpuacct", "usage_sys"));
+	} catch (...) {
+		PrintException(std::current_exception());
+	}
+
+	try {
+		position += sprintf(buffer + position,
+				    " memory.max_usage_in_bytes=%" PRIu64,
+				    ReadCgroupNumber(relative_path, "memory",
+						     "max_usage_in_bytes"));
+	} catch (...) {
+		PrintException(std::current_exception());
+	}
+
+	if (position > 0)
+		fprintf(stderr, "%s:%.*s\n", suffix, int(position), buffer);
+}
+
 static void
 DestroyCgroup(const CgroupState &state, const char *relative_path)
 {
-	// TODO: collect statistics
-
 	for (const auto &mount : state.mounts) {
 		char buffer[4096];
 		snprintf(buffer, sizeof(buffer), "/sys/fs/cgroup/%s%s",
@@ -56,6 +136,9 @@ Instance::OnSystemdAgentReleased(const char *path)
 		return;
 
 	printf("Cgroup released: %s\n", path);
+
+	CollectCgroupStats(path, suffix);
+	fflush(stdout);
 
 	// TODO: delay this call?
 	DestroyCgroup(cgroup_state, path);
