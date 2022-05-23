CM4all/spawn

commit ae07cf9e47653362400667160d98951c11c2d166

spawn/CgroupOptions: GetUnifiedMount() returns FileDescriptor

src/Instance.cxx
@@ -82,8 +82,8 @@ Instance::Instance()
 	if (!cgroup_state.IsEnabled())
 		throw std::runtime_error("systemd cgroups are not available");
 
-	const auto unified_mount = cgroup_state.GetUnifiedMount();
-	if (!unified_mount.empty()) {
+	if (const auto unified_mount = cgroup_state.GetUnifiedMountPath();
+	    !unified_mount.empty()) {
 		unified_cgroup_watch = std::make_unique<UnifiedCgroupWatch>(event_loop,
 									    unified_mount.c_str(),
 									    BIND_THIS_METHOD(OnSystemdAgentReleased));

src/Released.cxx
@@ -200,17 +200,8 @@ CollectCgroupStats(const char *relative_path, const char *suffix,
 
 	bool have_cpu_stat = false;
 
-	UniqueFileDescriptor v2_mount;
-	if (const auto v2_mount_path = state.GetUnifiedMount();
-	    !v2_mount_path.empty()) {
-		try {
-			v2_mount = OpenPath(v2_mount_path.c_str());
-		} catch (...) {
-			PrintException(std::current_exception());
-		}
-	}
-
-	if (v2_mount.IsDefined()) {
+	if (const auto v2_mount = state.GetUnifiedMount();
+	    v2_mount.IsDefined()) {
 		try {
 			const auto cpu_stat = ReadCgroupCpuStat(v2_mount,
 								relative_path);

commit 2ebc8a516d54775842e7802bb1f079469bd8a019

Instace: don't create systemd scope
Fixes another regression from commit
ae07cf9e47653362400667160d98951c11c2d166

src/Instance.cxx
@@ -69,11 +69,9 @@ Instance::Instance()
 	:shutdown_listener(event_loop, BIND_THIS_METHOD(OnExit)),
 	 sighup_event(event_loop, SIGHUP, BIND_THIS_METHOD(OnReload)),
 	 listener(event_loop, *this),
-	 cgroup_state(CreateSystemdScope("spawn.scope",
-					 "Process spawner helper daemon",
-					 {},
-					 getpid(), true,
-					 "system-cm4all.slice")),
+	 /* kludge: opening "/." so CgroupState contains file
+	    descriptors to the root cgroup */
+	 cgroup_state(CgroupState::FromProcess(0, "/.")),
 	 defer_cgroup_delete(event_loop,
 			     BIND_THIS_METHOD(OnDeferredCgroupDelete))
 {